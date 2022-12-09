ee067625d88722c408014f82bac4914cc993baec
Newproject011 <[200~keerthimasmile@gmail.com~>
2022-12-09 15:19:10 +0530

    going to push my code in the repository

---
+++ src/test/java/org/tcs/test/login/TrialClass.java
@@ -0,0 +1,20 @@
+package org.tcs.test.login;
+
+public class TrialClass {
+	private void classa() {
+		System.out.println("a");
+	}
+	private void classb() {
+		System.out.println("b");
+
+	}
+	private void classc() {
+		System.out.println("c");
+
+	}
+	private void classd() {
+		System.out.println("d");
+
+	}
+
+}
---
+++ src/test/java/org/tcs/test/login/package-info.java
@@ -0,0 +1,1 @@
+package org.tcs.test.login;
